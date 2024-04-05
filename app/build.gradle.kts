c58a73b0fde8749ec6e4fcb90769085c64936a61
Diego <diego.rof@hotmail.com>
2024-04-05 15:36:52 -0300

    Added viewmodel and navigation libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,11 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+
+    //ViewModels
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
