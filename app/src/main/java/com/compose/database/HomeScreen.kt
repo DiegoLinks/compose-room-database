77ff34c4b2bd9b8dfd87fa1c4b5eb3336e737741
Diego <diego.rof@hotmail.com>
2024-04-08 10:04:56 -0300

    Created Home Screen

---
+++ app/src/main/java/com/compose/database/HomeScreen.kt
@@ -0,0 +1,176 @@
+package com.compose.database
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.compose.database.ui.theme.ComposeRoomDBTheme
+
+object HomeDestination : NavigationDestination {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+/**
+ * Entry route for Home screen
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    navigateToItemUpdate: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val homeUiState by viewModel.homeUiState.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            PhonebookTopAppBar(
+                title = stringResource(HomeDestination.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.contact_entry_title)
+                )
+            }
+        },
+    ) { innerPadding ->
+        HomeBody(
+            contactList = homeUiState.contactList,
+            onItemClick = navigateToItemUpdate,
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+
+@Composable
+private fun HomeBody(
+    contactList: List<Contact>, onItemClick: (Int) -> Unit, modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (contactList.isEmpty()) {
+            Text(
+                text = stringResource(R.string.no_contact_description),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+            InventoryList(
+                contactList = contactList,
+                onItemClick = { onItemClick(it.id) },
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+
+@Composable
+private fun InventoryList(
+    contactList: List<Contact>, onItemClick: (Contact) -> Unit, modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(items = contactList, key = { it.id }) { contact ->
+            PhonebookContact(contact = contact,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+                    .clickable { onItemClick(contact) })
+        }
+    }
+}
+
+@Composable
+private fun PhonebookContact(
+    contact: Contact, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.padding_large))
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            Text(
+                text = contact.name,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeBodyPreview() {
+    ComposeRoomDBTheme {
+        HomeBody(listOf(
+            Contact(1, "Anna", "+5512345678", "anna@mail.com"),
+            Contact(2, "James", "+5512345679", "james@mail.com"),
+            Contact(3, "João", "+5512345670", "joao@mail.com")
+        ), onItemClick = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeBodyEmptyListPreview() {
+    ComposeRoomDBTheme {
+        HomeBody(listOf(), onItemClick = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PhonebookContactPreview() {
+    ComposeRoomDBTheme {
+        PhonebookContact(
+            Contact(1, "Anna", "+5512345678", "anna@mail.com"),
+        )
+    }
+}
