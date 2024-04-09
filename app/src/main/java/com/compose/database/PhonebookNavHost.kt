0120679e73b7122826aed28cbeaac9e95e71061a
Diego <diego.rof@hotmail.com>
2024-04-09 10:33:58 -0300

    Created a NavHost

---
+++ app/src/main/java/com/compose/database/PhonebookNavHost.kt
@@ -0,0 +1,27 @@
+package com.compose.database
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+
+/**
+ * Provides Navigation graph for the application.
+ */
+@Composable
+fun PhonebookNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = HomeDestination.route) {
+            HomeScreen()
+        }
+    }
+}
