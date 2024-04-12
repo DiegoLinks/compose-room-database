baccd8adb1b8e4171df123ee5f393ebb7edbb29b
Diego <diego.rof@hotmail.com>
2024-04-12 10:13:12 -0300

    Created HomeViewModel

---
+++ app/src/main/java/com/compose/database/HomeViewModel.kt
@@ -0,0 +1,25 @@
+package com.compose.database
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(contactRepository: ContactRepository) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> =
+        contactRepository.getAllItemsStream().map { HomeUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = HomeUiState()
+            )
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+data class HomeUiState(val contactList: List<Contact> = listOf())
