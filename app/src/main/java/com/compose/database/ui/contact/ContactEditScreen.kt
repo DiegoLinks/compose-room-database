cf94797b3b486a942d4706ffb4851c5461af755b
Diego <diego.rof@hotmail.com>
2024-04-24 16:20:20 -0300

    Created contact screen

---
+++ app/src/main/java/com/compose/database/ui/contact/ContactEditScreen.kt
@@ -0,0 +1,80 @@
+package com.compose.database.ui.contact
+
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.compose.database.PhonebookTopAppBar
+import com.compose.database.R
+import com.compose.database.ui.AppViewModelProvider
+import com.compose.database.ui.navigation.NavigationDestination
+import com.compose.database.ui.theme.ComposeRoomDBTheme
+import kotlinx.coroutines.launch
+
+object ItemEditDestination : NavigationDestination {
+    override val route = "contact_edit"
+    override val titleRes = R.string.contact_item_title
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContactEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: ContactEditViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            PhonebookTopAppBar(
+                title = stringResource(ItemEditDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        ContactEntryBody(
+            contactUiState = viewModel.contactUiState,
+            onItemValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                // Note: If the user rotates the screen very fast, the operation may get cancelled
+                // and the item may not be updated in the Database. This is because when config
+                // change occurs, the Activity will be recreated and the rememberCoroutineScope will
+                // be cancelled - since the scope is bound to composition.
+                coroutineScope.launch {
+                    //viewModel.updateItem()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(
+                    start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                    top = innerPadding.calculateTopPadding(),
+                    end = innerPadding.calculateEndPadding(LocalLayoutDirection.current),
+                )
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemEditScreenPreview() {
+    ComposeRoomDBTheme {
+        ContactEditScreen(navigateBack = { /*Do nothing*/ }, onNavigateUp = { /*Do nothing*/ })
+    }
+}
