8ab54178345f2eea2c79d7d4966914d9ee0fccde
Diego <diego.rof@hotmail.com>
2024-05-08 14:23:06 -0300

    Initializing the edit and home viewmodels

--- app/src/main/java/com/compose/database/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/compose/database/ui/AppViewModelProvider.kt
@@ -2,20 +2,34 @@ package com.compose.database.ui
 
 import android.app.Application
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.compose.database.PhonebookApplication
+import com.compose.database.ui.contact.ContactEditViewModel
 import com.compose.database.ui.contact.ContactEntryViewModel
+import com.compose.database.ui.home.HomeViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
+        initializer {
+            ContactEditViewModel(
+                this.createSavedStateHandle(),
+                inventoryApplication().container.contactRepository
+            )
+        }
+
         initializer {
             ContactEntryViewModel(inventoryApplication().container.contactRepository)
         }
 
         initializer {
             ContactEntryViewModel(inventoryApplication().container.contactRepository)
+        }
+
+        initializer {
+            HomeViewModel(inventoryApplication().container.contactRepository)
         }
     }
 
